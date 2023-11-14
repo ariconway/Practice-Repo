9368f0b1a095932b50f27832ff5aa3a8a03e411f
ariconway <41766988+ariconway@users.noreply.github.com>
2023-11-14 10:48:30 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,17 @@
 ﻿//ari_conway
-//Datatypes in C#
+//Arrays
 
-short aShortMinVal = short.MinValue;
-short aShortMaxVal = short.MaxValue;
+//float [] temperatures;
+//float[] temperatures = { 0.5f, 1, 2, -1.5f, 3, 2, 0 };
+//float[] temperatures = new float[]{ 0.5f, 1, 2, -1.5f, 3, 2, 0 };
+float[] temperatures = new float[7] { 0.5f, 1, 2, -1.5f, 3, 2, 0 };
 
-Console.WriteLine("aShortMinVal = " + aShortMinVal);
-Console.WriteLine("aShortMaxVal = " + aShortMaxVal);
-Console.WriteLine(aShortMinVal + "<= aShortNum <= " + aShortMaxVal);
+foreach (float temp in temperatures)
+{
+    Console.WriteLine(temp);
+}
 
-Int64 aLongMinVal = Int64.MinValue;
-Int64 aLongMaxVal = Int64.MaxValue;
+//temperatures = new float[20];
 
-Console.WriteLine("aLongMinVal = " + aLongMinVal);
-Console.Write("aShortMinVal = " + aShortMinVal);
-Console.WriteLine(aLongMinVal + "<= aLongNum <= " + aLongMaxVal);
 
-//arithemtic operators: + - * / %
 
-int a = 10;
-int b = 5;
-int c = 3;
-
-int sum_ab = a + b; //10 + 5 = 15
-int diff_ab = a - b; //10 - 5 = 5
-int diff_ba = b - a; //5 - 10 = -5
-int product_ab = a * b; //10 * 5 = 50
-int quotient_ab = a / b; //10 / 5 = 2
-int quotient_ac = a / c; //10 / 3 = 3.33333
-int remainder_ab = a % b; //remainder of 10 / 5 = 0
-int remainder_ac = a % b; //remainder of 10 / 3 = 1
-
-Console.WriteLine("Hello, we are calculating your final average.");
-Console.Write("Please enter your midterm grade: ");
-
-float midTermGrade = Console.ReadLine();
